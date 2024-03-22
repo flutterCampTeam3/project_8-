95e989cbb91aeb1c7cc2382c8142c5f770d2b7bd
khaled_alghamdi <khaledtabah@gmail.com>
2024-03-22 04:59:46 +0300

    chatAi

--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -25,7 +25,37 @@ class UserModel {
     return _data;
   }
 }
+//-----------
+/*
 
+class UserModel {
+  late final String id;
+  late final DateTime createdAt;
+  late final String name;
+
+  UserModel({
+    required this.id,
+    required this.createdAt,
+    required this.name,
+  });
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      createdAt: DateTime.parse(map['created_at']),
+      name: map['name'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'created_at': createdAt.toIso8601String(),
+      'name': name,
+    };
+  }
+}
+*/
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:project_8/data/service/supabase_configration.dart';
+import 'package:project_8/pages/ai%20chat%20page/chat_screen.dart';
 import 'package:project_8/pages/splach%20page/splach_page.dart';
 
 void main() async {
@@ -18,6 +19,6 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
         theme: ThemeData(fontFamily: GoogleFonts.vazirmatn().fontFamily),
         home: const Directionality(
-            textDirection: TextDirection.rtl, child: SplashPage()));
+            textDirection: TextDirection.rtl, child: ChatPage()));
   }
 }
